186507a7de66c9b8953fcda6297efd48883cd467
vaishaliusha <45840295+vaishaliusha@users.noreply.github.com>
2018-12-13 13:59:46 +0530

    Create logvalidation.js

---
+++ logvalidation.js
@@ -0,0 +1,13 @@
+log validation.js
+x=document.get element by ict("email")value;
+if("x==")
+{
+alert("email must be filled");
+return false:
+}
+x=document.get element by id("password")values;
+if("x==")
+{
+alert("password must be filled");
+return false;
+}
