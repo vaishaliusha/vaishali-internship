a442490e9cffe36aa149e7888ca5c116d8d8fc0b
vaishaliusha <45840295+vaishaliusha@users.noreply.github.com>
2018-12-13 14:00:40 +0530

    Create Registration.js

---
+++ Registration.js
@@ -0,0 +1,36 @@
+log valid form()
+{
+x=document.get element by ict("email")value;
+if("x==")
+{
+alert("email must be filled");
+return false:
+}
+x=document.get element by id("password")values;
+if("x==")
+{
+alert("password must be filled");
+return false;
+}
+x=document.get element by id("user name")values;
+if("x==")
+{
+alert("user name must be filled");
+return false;
+}x=document.get element by id("register no")values;
+if("x==")
+{
+alert("register no must be filled");
+return false;
+}x=document.get element by id("college name")values;
+if("x==")
+{
+alert("college name must be filled");
+return false;
+}x=document.get element by id("Degree")values;
+if("x==")
+{
+alert("Degree must be filled");
+return false;
+}
+}
